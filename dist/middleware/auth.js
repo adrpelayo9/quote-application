0469e7852e16a572657704046cf5d4f3c842af52
Adrian P <adrian_pelayo99@yahoo.com>
2024-02-11 05:28:10 -0800

    initial commit

---
+++ dist/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import env from 'dotenv';
+env.config();
+const auth = (req, res, next) => {
+    try {
+        const JWT_SECRET = process.env.JWT_SECRET;
+        const authHeader = req.headers.authorization;
+        const token = authHeader.split(' ')[1];
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch (err) {
+        res.status(401).json({ error: 'You are unauthenticated. Please login again. ' });
+    }
+};
+export const isAdmin = (req, res, next) => {
+    if (!req.user.roles.includes('ADMIN')) {
+        return res.status(403).json({ error: "You are not allowed to do this action." });
+    }
+    next();
+};
+export default auth;
---
+++ src/middleware/auth.ts
@@ -0,0 +1,28 @@
+import { RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
+import env from 'dotenv';
+env.config();
+
+const auth: RequestHandler = (req, res, next) => {
+    try {
+        const JWT_SECRET = process.env.JWT_SECRET!;
+        const authHeader = req.headers.authorization!;
+        const token = authHeader.split(' ')[1];
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+
+        next();
+    } catch (err) {
+        res.status(401).json({ error: 'You are unauthenticated. Please login again. '})
+    }
+};
+
+export const isAdmin: RequestHandler = (req, res, next) => {
+    if (!req.user.roles.includes('ADMIN')) {
+        return res.status(403).json({ error: "You are not allowed to do this action." });
+    }
+    next();
+};
+
+export default auth;
