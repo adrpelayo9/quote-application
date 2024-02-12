2db66d14220313014c41c18ccab4da6893765f31
Adrian P <adrian_pelayo99@yahoo.com>
2024-02-12 04:31:40 -0800

    trying to update backend

--- backend/dist/routes/auth.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import * as authController from '../controllers/auth.js';
-const router = Router();
-router.post('/login', authController.login);
-router.post('/register', authController.register);
-export default router;
